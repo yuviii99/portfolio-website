7162c3abe117146175959eb613c562a9d8b1ea95
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-06-19 17:59:07 +1000

    Release 1

---
+++ src/data/constants.js
@@ -0,0 +1,462 @@
+import django_rest_image from '../images/Django_REST.png';
+import three_js_image from '../images/three_js.png';
+import colab_image from '../images/colab.png';
+import anu_logo from '../images/anu_logo.png';
+import voxogenix_logo from '../images/voxogenic_logo.png';
+import iiit_logo from '../images/iiitvadodara_logo.jpeg';
+
+export const Bio = {
+  name: "Yuvraj Singh Rana",
+  roles: [
+    "Software Engineer",
+    "Full Stack Developer",
+    "API Developer",
+    "Web Developer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/yuviii99",
+  resume:
+    "https://drive.google.com/file/d/1SGKKj7P-fVecjLazLpSO8SWMGRvTGaEh/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/yuvraj99/",
+};
+
+export const skills = [
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+      },
+      {
+        name: "C",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg",
+      },
+      {
+        name: "C++",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
+      },
+      {
+        name: "Dart",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dart/dart-original.svg",
+      },
+      {
+        name: "JavaScript",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      },
+      {
+        name: "Ruby",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ruby/ruby-original.svg",
+      },
+      {
+        name: "Bash",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg",
+      },
+      {
+        name: "R",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/r/r-plain.svg",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Django",
+        image: "https://cdn.iconscout.com/icon/free/png-256/free-django-1-282754.png",
+      },
+      {
+        name: "DjangoRest",
+        image: django_rest_image,
+      },
+      {
+        name: "Flask",
+        image:
+          "https://img.icons8.com/?size=100&id=5mbMwDZ796xj&format=png&color=000000",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
+      },
+      {
+        name: "NodeJS",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
+      },
+      {
+        name: "mongoose",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original.svg",
+      },
+      {
+        name: "Express",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg",
+      },
+      {
+        name: "FastAPI",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg",
+      },
+      {
+        name: "Maven",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/maven/maven-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      },
+      {
+        name: "MaterialUI",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg",
+      },
+      {
+        name: "CSS3",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
+      },
+      {
+        name: "Bootstrap",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg",
+      },
+      {
+        name: "Flutter",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flutter/flutter-original.svg",
+      },
+      {
+        name: "SCSS",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
+      },
+      {
+        name: "ThreeJS",
+        image: three_js_image,
+      },
+      {
+        name: "jQuery",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original.svg",
+      },
+      {
+        name: "Figma",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg",
+      }
+    ]
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Google Cloud",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original.svg",
+      },
+      {
+        name: "JIRA",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jira/jira-original.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jenkins/jenkins-original.svg",
+      },
+      {
+        name: "Nginx",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nginx/nginx-original.svg",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg",
+      },
+      {
+        name: "GitLab",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original.svg",
+      },
+      {
+        name: "Selenium",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/selenium/selenium-original.svg",
+      },
+      {
+        name: "Jekyll",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jekyll/jekyll-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+      },
+      {
+        name: "PostgreSQL",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg",
+      },
+      {
+        name: "SQLite",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg",
+      },
+      {
+        name: "SQLAlchemy",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlalchemy/sqlalchemy-original.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-plain-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original.svg",
+      },
+      {
+        name: "Supabase",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/supabase/supabase-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Data Science",
+    skills: [
+      {
+        name: "Numpy",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/numpy/numpy-original.svg",
+      },
+      {
+        name: "Pandas",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original.svg",
+      },
+      {
+        name: "Keras",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/keras/keras-original.svg",
+      },
+      {
+        name: "Sklearn",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg",
+      },
+      {
+        name: "Matplotlib",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matplotlib/matplotlib-original.svg",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jupyter/jupyter-original-wordmark.svg",
+      },
+      {
+        name: "Google Colab",
+        image: colab_image,
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: anu_logo,
+    role: "Software Engineer",
+    company: "The Australian National University",
+    date: "Jan 2024 - Present",
+    desc: "Played a key role in development of ANU College of Engineering, Computing and Cybernetics new website. Designed a cohesive layout for all webpages, ensuring a consistent and user-friendly experience for visitors. Managed and optimized the website's backend configuration using Netlify's CMS for efficient content management. Ensured the successful execution of CI/CD pipeline while deploying updates. Developed custom Ruby plugins to enhance website functionality and improve user experience.",
+    skills: [
+      "jekyll",
+      "Ruby",
+      "Netlify CMS",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "GitLab DevOps"
+    ],
+  },
+  {
+    id: 1,
+    img: anu_logo,
+    role: "Software Engineer Intern",
+    company: "ANU School of Computing",
+    date: "Jul 2023 - Jan 2024",
+    desc: "Streamlined student graduation for 15+ ANU College of Engineering, Computing and Cybernetics (CECC) degree programs. Built a graduation validation tool with 100% accuracy, incorporating sophisticated algorithms to handle the flexibility of ANU programs. Utilized Flask for the backend development, ensuring efficient data processing. Leveraged modern frontend technologies to create a user-friendly and intuitive interface for student services staff.",
+    skills: [
+      "Python",
+      "Flask",
+      "pandas",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ]
+  },
+  {
+    id: 2,
+    img: voxogenix_logo,
+    role: "Software Development Engineer",
+    company: "Voxogenic Technologies",
+    date: "Jun 2021 - July 2022",
+    desc: "Developed robust APIs and web applications using Django, Django REST Framework fostering seamless communication and functionality across diverse platforms. Optimized existing backend systems, achieving a remarkable 40% improvement in performance and reliability, ensuring a smooth user experience. Engineered Alexa Skills, integrating advanced voice command features into IoT systems. This empowered users with a natural and intuitive way to interact, boosting engagement.",
+    skills: [
+      "Python",
+      "Django",
+      "Flask",
+      "REST APIs",
+      "PostgreSQL",
+      "MongoDB",
+      "Figma",
+      "Alexa Skills",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+  },
+  {
+    id: 3,
+    img: iiit_logo,
+    role: "Undergraduate Research Assistant",
+    company: "Indian Institute of Information Technology, Vadodara",
+    date: "Jan 2021 - May 2021",
+    desc: "Investigated and identified critical security vulnerabilities within AADHAR, India's national identity management system. Developed solutions employing distributed computing and decentralisation techniques to mitigate risks of data breaches. Enhanced the system's security framework, laying the groundwork for more robust protection against potential data leaks.",
+    skills: [
+      "Cryptography",
+      "Blockchain",
+      "Distributed Systems Security",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: anu_logo,
+    school: "The Australian National University",
+    date: "Jul 2022 - Jun 2024",
+    desc: "My coursework included core subjects such as Software Construction, Relational Databases and Structured Programming along with advanced topics like Machine Learning, Data Mining, Data Wrangling and Document Analysis.",
+    degree: "Master of Computing - Specialisation in Data Science",
+  },
+  {
+    id: 1,
+    img: iiit_logo,
+    school: "Indian Institute of Information Technology, Vadodara",
+    date: "Aug 2017 - Jul 2021",
+    desc: "My coursework included basics of C Programming, Data Structures and Algorithms, Computer Networks along with advanced subjects like Cloud Computing, Distributed Systems and Software Engineering.",
+    degree: "Bachelor of Technology in Computer Science and Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "FlexFit",
+    description:
+      "A React application integrated with RapidAPI and YouTube API, designed to assist you with daily exercises at gym or home. It provides user the functionality to search for different exercises or body parts and display a GIF for each exercise. It also shows the top 3 YouTube videos for an exercise.",
+    image:
+      "https://raw.githubusercontent.com/yuviii99/flexfit/master/screenshots/search_area.png",
+    github: "https://github.com/yuviii99/flexfit",
+  },
+  {
+    id: 9,
+    title: "NYC Taxi Data Analytics",
+    description:
+      "Data engineering pipeline using MageAI supplying NYC TLC Rideshare data through Google BigQuery to looker studio dashboard to perform analytics.",
+    image:
+      "https://raw.githubusercontent.com/yuviii99/nyc-tlc-data-analytics/master/images/dashboard.png",
+    github: "https://github.com/yuviii99/nyc-tlc-data-analytics",
+  },
+  {
+    id: 0,
+    title: "Daily Digest",
+    description:
+      "A flutter based news application that fetches real time news from NewsAPI.  Enjoy a clean and user-friendly interface while staying informed with the latest headlines and breaking news delivered straight from trusted sources around the globe.",
+    image:
+      "https://raw.githubusercontent.com/yuviii99/dailydigest/master/screenshots/Screenshot%202024-06-19%20at%204.37.30%20pm.png",
+    github: "https://github.com/yuviii99/dailydigest",
+  },
+  {
+    id: 4,
+    title: "API : Recipe App",
+    description:
+      "An django REST API that allows authentication, storing, updating, fetching and deleting of any food recipe on the cloud. It can be integrated with any kind of mobile/web application.",
+    image:
+      "https://images.unsplash.com/photo-1623282033815-40b05d96c903?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    github: "https://github.com/yuviii99/recipe-app-api",
+  },
+  {
+    id: 2,
+    title: "Cloud Notebook",
+    description:
+      "A MERN application allowing you to sign up/sign in using JWT auth, creating and deleting notes which are stored in mongoDB database on the cloud.",
+    image:
+      "https://raw.githubusercontent.com/yuviii99/cloud-notebook/master/screenshots/add_note.png",
+    github: "https://github.com/yuviii99/cloud-notebook",
+  },
+  {
+    id: 3,
+    title: "PalPulse",
+    description:
+      "A flutter based real time chat application that lets user send and recieve text messages and attachments. The backend is built on the supabase, which is a popular alternate to FireBase.",
+    image:
+      "https://raw.githubusercontent.com/yuviii99/palpulse/master/screenshots/palpulse_web_ss.png",
+    github: "https://github.com/yuviii99/palpulse",
+  },
+  {
+    id: 1,
+    title: "Pixel Racers",
+    description:
+      "Rev your engines for Pixel Racers, a retro-inspired car racing game built with Python and Pygame! Race pixel-perfect cars on a fast-paced track, Buckle up and get ready for some pixelated racing action!",
+    image:
+      "https://raw.githubusercontent.com/yuviii99/pixel-racers/master/pixel_racers_web_ss.png",
+    github: "https://github.com/yuviii99/pixel-racers",
+  },
+  {
+    id: 5,
+    title: "Crypto Price Notifier",
+    description:
+      "A CRUD Flask based web app that uses Coin Market Cap API to fetch the real-time prices of cryptocurrencies added in our wish list. The data of our added crypto is stored in a SQLite Database connected to our program using SQLAlchemy. If the price of the crypto changes by a certain percentage in the last 24 hours, the app sends an email to the user using SMTP.",
+    image:
+      "https://raw.githubusercontent.com/yuviii99/crypto-price-notifier/master/home.png",
+    github: "https://github.com/yuviii99/crypto-price-notifier",
+  },
+  {
+    id: 6,
+    title: "PixelQR",
+    description:
+      "A web application using which event hosts can provide images to the people that attended the event in a personalised way. Built with the backend of Python-Flask with a focus on maintaining privacy of the users by tagging the images with a unique ID so that only the person with the ID can access the images.",
+    image:
+      "https://raw.githubusercontent.com/yuviii99/PixelQR/master/images/Screenshot%202024-06-19%20at%205.45.17%20pm.png",
+    github: "https://github.com/yuviii99/PixelQR",
+  },
+  
+];
